7deff95410bf165bc9ba028d9f95bd634b2085a0
Zaur Mamedov <zaurmd1996@gmail.com>
2024-02-11 12:25:12 +0300

    Reapply "Added '!!!'"
    This reverts commit 16d37e833749b6b79ef31f5abd70536abbf327d3.

--- main.py
+++ main.py
@@ -6,7 +6,7 @@
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hello, dear {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'Hello, dear {name}!!!')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
